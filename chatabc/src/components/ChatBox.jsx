0c7dc5e6b84c99fa0bae830d1fd2158c8c10a744
hikito-otikih <24125020@student.hcmus.edu.vn>
2025-11-16 11:17:36 +0700

    update chatbot.jsx-import

--- chatabc/src/components/ChatBox.jsx
+++ chatabc/src/components/ChatBox.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets';
-import Message from './message';
+import Message from './Message';
 import toast from 'react-hot-toast';
 
 const ChatBox = () => {
